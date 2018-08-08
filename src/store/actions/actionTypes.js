a47ee91a9cebe39b827e0b5a9f4588ade4eca7f9
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-08 17:53:27 +0530

    action types added

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const FETCH_AUTH_SUCCESS = 'FETCH_AUTH_SUCCESS';
+export const FETCH_AUTH_FAIL = 'FETCH_AUTH_FAIL';
+
+export const ADD_NOTE = 'ADD_NOTE';
+export const SET_ACTIVE_NOTE = 'SET_ACTIVE_NOTE'; 
+export const SET_UNACTIVE = 'SET_UNACTIVE';
